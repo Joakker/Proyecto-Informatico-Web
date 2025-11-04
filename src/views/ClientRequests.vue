020eaea8533b2e045c89de77abf0a39e28d79996
Bastián <bas_bece@hotmail.com>
2025-11-04 13:06:41 -0300

    workrequest cards working

--- src/views/ClientRequests.vue
+++ src/views/ClientRequests.vue
@@ -1,22 +1,23 @@
 
 <script setup lang="ts">
     import { ref, onMounted } from 'vue'
-    import WorkCard from '../components/ClientRequestCard.vue'
+    import WorkRequestCard from '../components/ClientRequestCard.vue'
 
-    interface Work {
+    interface WorkRequest {
         id: number
         title: string
         description: string
         price: number
     }
 
-    const works = ref<Work[]>([])
+    const works = ref<WorkRequest[]>([])
     const loading = ref(true)
 
     
     onMounted(async () => {
         try {
-            const response = await fetch('http://127.0.0.1:8000/api/works')
+            
+            const response = await fetch('http://127.0.0.1:8000/api/clientrequests')
             const data = await response.json()
             works.value = data
         } catch (error) {
@@ -29,10 +30,10 @@
 
 <template>
   <div>
-    <h2>Available Works</h2>
-    <div v-if="loading">Loading...</div>
+    <h2>Pedidos</h2>
+    <div v-if="loading">Cargando pedidos...</div>
     <div v-else>
-      <WorkCard v-for="work in works" :key="work.id" :work="work" />
+      <WorkRequestCard v-for="work in works" :key="work.id" :work="work" />
     </div>
   </div>
 </template>
